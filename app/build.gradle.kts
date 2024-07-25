4b517472aaa606b84d07a322042fde7e2c8c1651
mel <melanieromero7111@gmail.com>
2024-07-25 04:23:49 -0300

    Screen Splash añadido

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,15 +41,16 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+//API splashscreen
+    implementation ("androidx.core:core-splashscreen:1.0.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation ("com.github.jose-jhr:blueJhrLibrary:0.1.0")
 }
--- app/src/main/java/com/example/audilu/audiluActivity.kt
+++ app/src/main/java/com/example/audilu/audiluActivity.kt
@@ -3,12 +3,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class audiluActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audilu)
-
-
     }
 }
 
