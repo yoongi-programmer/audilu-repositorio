7bf0bcc75b55c54c6d625a23d90661403f7e6b0a
mel <melanieromero7111@gmail.com>
2024-07-26 21:45:53 -0300

    Nueva Funcion: Preferencias settings,layout terminada

---
+++ app/src/main/java/com/example/audilu/preferenciasFragment.kt
@@ -0,0 +1,28 @@
+package com.example.audilu
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import com.example.audilu.databinding.FragmentPreferenciasBinding
+
+class preferenciasFragment : Fragment() {
+
+    private var _binding: FragmentPreferenciasBinding? = null
+    private val binding get() = _binding!!
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Infla el layout para este fragmento con el método binding
+        _binding = FragmentPreferenciasBinding.inflate(inflater, container, false)
+        val root = binding.root
+
+        return root
+    }
+}
+
